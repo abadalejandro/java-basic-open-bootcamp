ca2b965873e7f50865ec54d42d9dcd145fa9fc8b
abadalejandro <36165616+abadalejandro@users.noreply.github.com>
2022-10-01 17:18:20 +0200

    Ejercicio_01 => ok

---
+++ src/main/java/com/ejercicios/Ejercicio_01.java
@@ -0,0 +1,18 @@
+package com.ejercicios;
+
+public class Ejercicio_01 {
+    public static void main(String[] args) {
+        int myInteger = 2147483647;
+        long myLongInteger = 9223372036854775807L;
+        double myDouble = 1.79769313486232e30;
+        boolean isAdult = true;
+        String gretting = "Hello World!!!";
+
+
+        System.out.println("int: " + myInteger);
+        System.out.println("long: " + myLongInteger);
+        System.out.println("dobule: " + myDouble);
+        System.out.println("boolean: " + isAdult);
+        System.out.println("string: " + gretting);
+    }
+}
